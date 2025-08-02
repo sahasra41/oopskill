272266aec60e80eb5f87bc0c9e065f28a17409c1
sahasra41 <pagadalagodasahasra@gmail.com>
2025-08-02 11:15:22 +0530

    Add files via upload

---
+++ CaseConversion.java
@@ -0,0 +1,9 @@
+package oop_skill;
+
+public class CaseConversion {
+	   public static void main(String[] args) {
+	        String s = "hello world";
+	        System.out.println(s.toUpperCase());   
+	        System.out.println(s.toLowerCase());   
+	    }
+	}
---
+++ SearchIndex.java
@@ -0,0 +1,11 @@
+package oop_skill;
+
+public class SearchIndex {
+
+    public static void main(String[] args) {
+        String s = "Hello world";
+        System.out.println(s.indexOf(""));        
+        System.out.println(s.lastIndexOf("na"));    
+        System.out.println(s.contains("nan"));      
+    }
+}
