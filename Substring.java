763c60c2dd0c502320509ca15d62666e8542f740
sahasra41 <pagadalagodasahasra@gmail.com>
2025-08-02 11:11:22 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,12 @@
+package oop_skill;
+
+public class Substring {
+	 public static void main(String[] args) {
+	        String s = "Hello World";
+	        System.out.println(s.substring(5));         
+	        System.out.println(s.substring(2, 7));       
+	    }
+	}
+
+
+
